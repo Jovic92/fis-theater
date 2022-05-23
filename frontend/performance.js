302f457b82a650a6f2f2bbeffe753cfc24da0bd0
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-05-23 19:27:08 +0200

    T2 done

--- frontend/performance.js
+++ frontend/performance.js
@@ -2,7 +2,14 @@ Vue.createApp({
     data() {
         return {
             showNameForSearch: '',
-            performancesDateAndScene: []
+            performancesDateAndScene: [],
+            shows: [],
+            show: {
+                name: '',
+                description: '',
+                length: 0,
+                genere: []
+            }
         }
     },
     methods: {
@@ -10,15 +17,37 @@ Vue.createApp({
             axios
                 .get("http://localhost:8080/performance/date-and-scene/" + this.showNameForSearch)
                 .then(response => {
-                    console.log(response)
+                    // console.log(response)
                     this.performancesDateAndScene = response.data
                 })
                 .catch(function (error) {
                     console.log(error)
                     alert("AN ERROR HAS OCCURED")
                 })
+        },
+        setSelectedShow() {
+            this.shows.forEach(elem => {
+                // console.log(elem.name)
+                // console.log(this.showNameForSearch)
+                if (elem.name === this.showNameForSearch) {
+                    this.show = elem
+                    console.log(this.show)
+                }
+            })
+            
+            // = this.shows.
         }
     },
     mounted: function () {
+        axios
+            .get("http://localhost:8080/show/getAll/")
+            .then(response => {
+                console.log(response)
+                this.shows = response.data
+            })
+            .catch(function (error) {
+                console.log(error)
+                alert("AN ERROR HAS OCCURED")
+            })
     }
 }).mount("#perf")
