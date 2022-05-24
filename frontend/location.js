5a75b25414f04f23de3b1d9125ebe980a350a070
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-05-24 17:40:20 +0200

    T5 js done

--- frontend/location.js
+++ frontend/location.js
@@ -1,14 +1,25 @@
 Vue.createApp({
     data() {
-      return {
-        
-      }
+        return {
+            dateOfShow: '',
+            nameOfShow: '',
+            locations: []
+        }
     },
     methods: {
-      
+        show() {
+            axios
+                .get("htpp://localhost:8080/location/" + this.dateOfShow + "/" + this.nameOfShow)
+                .then(response => {
+                    console.log(response.data)
+                    this.locations = response.data
+                })
+                .catch(function (err) {
+                    alert("ERROR HAS HAPPENED")
+                })
+        }
     },
-    mounted: function() {
-      
+    mounted: function () {
+
     }
-  }).mount("#location")
-  
+}).mount("#location")
