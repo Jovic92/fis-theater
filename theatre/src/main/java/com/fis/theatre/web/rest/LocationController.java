77a2c3b5d3cd543f9b2d856b2742084216e601d2
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-05-24 17:35:07 +0200

    T5 html done

--- theatre/src/main/java/com/fis/theatre/web/rest/LocationController.java
+++ theatre/src/main/java/com/fis/theatre/web/rest/LocationController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.fis.theatre.service.LocationService;
 import com.fis.theatre.web.dto.LocationDTO;
 
 @RestController
+@CrossOrigin
 public class LocationController {
 
 	@Autowired
