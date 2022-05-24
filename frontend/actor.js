d82f7f95002081da3dd956060570adaa44ecf2b1
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-05-24 18:18:40 +0200

    T6 done

--- frontend/actor.js
+++ frontend/actor.js
@@ -1,14 +1,27 @@
 Vue.createApp({
     data() {
-      return {
-        
-      }
+        return {
+            actor: {
+                firstName: '',
+                lastName: ''
+            }
+        }
     },
     methods: {
-      
+        addActor() {
+            axios
+                .post("http://localhost:8080/actor", this.actor)
+                .then(resp => { 
+                    console.log(resp) 
+                    alert("An actor has been added")
+                })
+                .catch(function (erroreewafshrjd) {
+                    console.log(erroreewafshrjd)
+                    alert("ERROR OCCURED")
+                })
+        }
     },
-    mounted: function() {
-      
+    mounted: function () {
+
     }
-  }).mount("#act")
-  
+}).mount("#act")
