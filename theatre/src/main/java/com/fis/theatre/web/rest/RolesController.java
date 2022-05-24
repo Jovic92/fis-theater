8481cc46248603b77cac7eecc06d8e8a7a17af8b
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-05-24 18:40:26 +0200

    T8 html first part

--- theatre/src/main/java/com/fis/theatre/web/rest/RolesController.java
+++ theatre/src/main/java/com/fis/theatre/web/rest/RolesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.fis.theatre.service.RoleService;
 import com.fis.theatre.web.dto.RoleDTO;
 
 @RestController
+@CrossOrigin
 public class RolesController {
 
 	@Autowired
