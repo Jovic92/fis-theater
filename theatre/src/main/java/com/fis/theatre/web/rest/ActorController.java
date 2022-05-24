ab56ebfe46c27e81df43feb1f05a3bdaa3f185b2
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-05-24 18:13:28 +0200

    T6 html done

--- theatre/src/main/java/com/fis/theatre/web/rest/ActorController.java
+++ theatre/src/main/java/com/fis/theatre/web/rest/ActorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import com.fis.theatre.web.dto.ActorDTO;
 import com.fis.theatre.web.dto.RoleActorDTO;
 
 @RestController
+@CrossOrigin
 public class ActorController {
 
 	@Autowired
