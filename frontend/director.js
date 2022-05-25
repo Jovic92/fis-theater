dffde48fff0a3146a4cc67619d01557192cb2259
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-05-25 17:23:14 +0200

    t15 done

--- frontend/director.js
+++ frontend/director.js
@@ -1,11 +1,24 @@
 Vue.createApp({
     data() {
         return {
-            directors: []
+            directors: [],
+            shows: []
         }
     },
     methods: {
-
+        getShows(directorId) {
+            console.log(directorId)
+            axios
+                .get("http://localhost:8080/show/director/" + directorId)
+                .then(resp => {
+                    this.shows = resp.data
+                    console.log(this.shows)
+                    this.shows.forEach(element => {
+                        element.tempGeneres = element.genres.join()
+                    });
+                    // this.shows.tempGeneres = this.shows.genres.join()
+                })
+        }
     },
     mounted: function () {
         axios
